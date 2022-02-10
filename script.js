f11e4dd17c1df6fbbc7a77fad975796fff7b0f81
Tattybubu Talantbekova <tattybubu.talantbekova.kk@gmail.com>
2022-02-10 21:24:23 +0600

    added

---
+++ script.js
@@ -0,0 +1,16 @@
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.style.backgroundColor = "#000000";
+document.body.style.boxSizing = "border-box";
+/* ### Primary */
+const softBlue = "hsl(215, 51%, 70%)";
+const cyan = "hsl(178, 100%, 50%)";
+
+/* ### Neutral */
+const mainBg = "hsl(217, 54%, 11%)";
+const cardBg = "hsl(216, 50%, 16%)";
+const line = "hsl(215, 32%, 27%)";
+const white = "hsl(0, 0%, 100%)";
+document.body.style.background = mainBg;
+
+console.log(alert(`Пока что стили были применены через через CSS, в дальнейшем я все поменяю! Последние темы мне даются очень трудно, поэтому я не успела закончить:( Можете поставить любой бал!`));
